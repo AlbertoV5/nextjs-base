f8ddc992603c676dd6fb8a098d530864d37f2ca8
AlbertoV5 <avq5ac1@gmail.com>
2023-08-07 01:35:00 -0600

    fixed small window'

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,25 @@
-import { Metadata } from "next"
-import Image from "next/image"
-import Link from "next/link"
+import { Metadata } from 'next';
+import Image from 'next/image';
+import Link from 'next/link';
 
-import { cn } from "@/lib/utils"
-import { buttonVariants } from "@/components/new-york/ui/button"
-import { UserAuthForm } from "@/components/app/user-auth-form"
+import { cn } from '@/lib/utils';
+import { buttonVariants } from '@/components/new-york/ui/button';
+import { UserAuthForm } from '@/components/app/user-auth-form';
 
 export const metadata: Metadata = {
-  title: "Authentication",
-  description: "Authentication forms built using the components.",
-}
+  title: 'Authentication',
+  description: 'Authentication forms built using the components.',
+};
 
 export default function AuthenticationPage() {
   return (
     <>
-      <div className="container relative hidden h-[800px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+      <div className="container relative h-[800px] flex-col items-center justify-center pt-48 md:pt-0 md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
         <Link
           href="/examples/authentication"
           className={cn(
-            buttonVariants({ variant: "ghost" }),
-            "absolute right-4 top-4 md:right-8 md:top-8"
+            buttonVariants({ variant: 'ghost' }),
+            'absolute right-4 top-4 md:right-8 md:top-8'
           )}
         >
           Login
@@ -53,7 +53,7 @@ export default function AuthenticationPage() {
           </div>
         </div>
         <div className="lg:p-8">
-          <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+          <div className="mx-auto flex w-full flex-col justify-center space-y-6 ">
             <div className="flex flex-col space-y-2 text-center">
               <h1 className="text-2xl font-semibold tracking-tight">
                 Create an account
@@ -64,14 +64,14 @@ export default function AuthenticationPage() {
             </div>
             <UserAuthForm />
             <p className="px-8 text-center text-sm text-muted-foreground">
-              By clicking continue, you agree to our{" "}
+              By clicking continue, you agree to our{' '}
               <Link
                 href="/terms"
                 className="underline underline-offset-4 hover:text-primary"
               >
                 Terms of Service
-              </Link>{" "}
-              and{" "}
+              </Link>{' '}
+              and{' '}
               <Link
                 href="/privacy"
                 className="underline underline-offset-4 hover:text-primary"
@@ -84,5 +84,5 @@ export default function AuthenticationPage() {
         </div>
       </div>
     </>
-  )
+  );
 }
